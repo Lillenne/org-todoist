27640cf9e7d96675c34d13f45c77ffd47f71e5ec
Austin Kearns <59812315+Lillenne@users.noreply.github.com>
2025-07-06 17:13:01 -0700

    workflow and test comment

--- test.el
+++ test.el
@@ -1,23 +1,5 @@
-;;; test.el --- Tests for org-todoist -*- lexical-binding: t; -*-
-;;
-;; Copyright (C) 2024 Austin Kearns
-;;
-;; Author: Austin Kearns <59812315+Lillenne@users.noreply.github.com>
-;; Maintainer: Austin Kearns <59812315+Lillenne@users.noreply.github.com>
-;; Created: September 16, 2024
-;; Modified: September 16, 2024
-;; Version: 0.0.1
-;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
-;; Homepage: https://github.com/lillenne/org-todoist
-;; Package-Requires: ((emacs "29.1") (s "1.13.1") (org "9.7.19") (ts "0.3") (dash "2.19.1"))
-;;
-;; This file is not part of GNU Emacs.
-;;
-;;; Commentary:
-;;
-;;  Tests for org-todoist
-;;
-;;; Code:
+;; Unfortunately, these are out of date!
+
 (require 'ert)
 (require 'org-todoist)
 
